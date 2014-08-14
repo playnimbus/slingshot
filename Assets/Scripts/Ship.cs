b814264aab7829476d62971775732012476dfab3
Adam Roy <adam@playnimbus.com>
2014-08-14 01:21:18 -0400

    Better orbit entry.

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -3,11 +3,19 @@ using System.Collections;
 
 public class Ship : MonoBehaviour
 {
+    public float minSpeed;
+    public float maxSpeed;
+    [Range(0, 1)] public float accelerationLerp;
+    [Range(0, 1)] public float decelerationLerp;
+
     public float speed;
+
+    private float angle;
 
     void Start()
     {
-        StartCoroutine(MovementCoroutine(0));        
+        speed = minSpeed;
+        StartCoroutine(MovementCoroutine(Mathf.PI / 2f));
     }
     
 	IEnumerator MovementCoroutine(float angle)
@@ -16,8 +24,9 @@ public class Ship : MonoBehaviour
 
         while(true)
         {
-            speed -= Time.fixedDeltaTime;
-            speed = Mathf.Max(speed, 5f);
+            speed = Mathf.Lerp(speed, minSpeed, decelerationLerp);
+
+
             Vector3 positionDelta = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle)) * speed * Time.fixedDeltaTime;
             rigidbody2D.MovePosition(transform.position + positionDelta);
             rigidbody2D.MoveRotation((angle - Mathf.PI / 2f) * Mathf.Rad2Deg);
@@ -25,25 +34,43 @@ public class Ship : MonoBehaviour
         }
     }
 
+    float AngleDifferenceForDistance(float distance, float radius)
+    {
+
+        return 0f;
+    }
+
     IEnumerator OrbitCoroutine(Planet planet)
     {
+        /* Don't question any math, just go with it */
         WaitForFixedUpdate wait = new WaitForFixedUpdate();        
         Vector3 positionDiff = transform.position - planet.transform.position;
         float currentAngle = Mathf.Atan2(positionDiff.y, positionDiff.x);
         float startRadius = positionDiff.magnitude;
-        float time = 0f;
+
+        // 1 for clockwise, -1 for counter clockwise
+        float directionModifier = 1f;
+        if (transform.InverseTransformPoint(planet.transform.position).x > 0f)
+        {
+            directionModifier = -1f;
+        }
+
+        float currentRadius = startRadius;
+        float lerpModifier = Mathf.Abs(transform.InverseTransformDirection(positionDiff.y, -positionDiff.x, 0f).normalized.x);
+        print(lerpModifier);
+        float radiusLerp =  (speed * Time.fixedDeltaTime) / (startRadius - planet.orbitRadius);
+        radiusLerp *= lerpModifier * lerpModifier;
 
         bool startedLaunch = false;
-
-        while(time < 2f)
+        while (true)
         {
-            time += Time.fixedDeltaTime;
+            float percent = 1f - ((currentRadius - planet.orbitRadius) / (startRadius - planet.orbitRadius));
 
-            float lerpedRadius = Mathf.Lerp(startRadius, planet.orbitRadius, time/2f);
-            float angleDelta = speed / lerpedRadius;
-            currentAngle += angleDelta * Time.fixedDeltaTime;
+            currentRadius = Mathf.Lerp(currentRadius, planet.orbitRadius, radiusLerp);
+            float angleDelta = percent * (speed / currentRadius);
+            currentAngle += directionModifier * angleDelta * Time.fixedDeltaTime;
 
-            Vector3 position = new Vector3(Mathf.Cos(currentAngle), Mathf.Sin(currentAngle)) * lerpedRadius + planet.transform.position;
+            Vector3 position = new Vector3(Mathf.Cos(currentAngle), Mathf.Sin(currentAngle)) * currentRadius + planet.transform.position;
             Vector3 deltaPosition = position - transform.position;
 
             rigidbody2D.MovePosition(position);
@@ -51,38 +78,18 @@ public class Ship : MonoBehaviour
 
             if(planet.mouseDown)
             {
-                speed += Time.fixedDeltaTime * 4f;
+                speed = Mathf.Lerp(speed, maxSpeed, accelerationLerp);
                 startedLaunch = true;
+            }
+            else
+            {
+                speed = Mathf.Lerp(speed, minSpeed, decelerationLerp);
             }
             if(!planet.mouseDown && startedLaunch)
             {
                 // Launch
                 StopAllCoroutines();
-                StartCoroutine(MovementCoroutine(currentAngle + Mathf.PI / 2f));
-            }
-
-            yield return wait;
-        }
-
-        while (true)
-        {
-            float angleDelta = speed / planet.orbitRadius;
-            currentAngle += angleDelta * Time.fixedDeltaTime;
-            Vector3 position = new Vector3(Mathf.Cos(currentAngle), Mathf.Sin(currentAngle)) * planet.orbitRadius + planet.transform.position;
-            
-            rigidbody2D.MovePosition(position);
-            rigidbody2D.MoveRotation(currentAngle * Mathf.Rad2Deg);
-
-            if (planet.mouseDown)
-            {
-                speed += Time.fixedDeltaTime * 4f;
-                startedLaunch = true;
-            }
-            if (!planet.mouseDown && startedLaunch)
-            {
-                // Launch
-                StopAllCoroutines();
-                StartCoroutine(MovementCoroutine(currentAngle + Mathf.PI / 2f));
+                StartCoroutine(MovementCoroutine(currentAngle + directionModifier * Mathf.PI / 2f));
             }
 
             yield return wait;
